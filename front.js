64ae3ebd6f4348abfb551d80cb8aeb32af916030
Yuki Mitsui <silphire@gmail.com>
2012-11-25 21:21:04 +0900

    created File API user interface

--- front.js
+++ front.js
@@ -2,20 +2,40 @@
  * morpwx front.js --- frontend interface
  */
 
+var tcxFiles = [];
+
 function onDrop(event) {
   var files = event.dataTransfer.files;
   var elem = event.target || event.srcElement;
   if(!elem) { alert("cannot get elem"); }
 
   for(var i = 0; i < files.length; ++i) {
+    var fileName = files[i].name.replace(/\.pwx/, '.tcx');
     var reader = new FileReader();
     reader.onload = function(event) {
       var text = reader.result;
       var domParser = new DOMParser();
       var xml = domParser.parseFromString(text, 'application/xml');
       var pwx = new PWX();
-      var pwxJson = pwx.readFromXML(xml);
-      elem.value = pwx.writeToTCX();
+      pwx.readFromXML(xml);
+      var tcx = pwx.writeToTCX();
+
+      var blob = new Blob([tcx]);
+      var blobURL = (window.URL || window.webkitURL).createObjectURL(blob);
+
+      var filelist = document.getElementById('filelist');
+      var nChildElems = 0;
+      for(var i = 0; i < filelist.childNodes.length; ++i) {
+	if(filelist.childNodes[i] == Node.ELEMENT_NODE) 
+	  nChildElements += 1;
+      }
+      var tr = document.createElement('tr');
+      tr.innerHTML = '<td>' + pwx.time + '</td>'
+	+ '<td>' + pwx.summarydata.duration + '</td>'
+	+ '<td>' + pwx.sportType + '</td>'
+	+ '<td>' + pwx.summarydata.dist + '</td>'
+	+ '<td><a href="' + blobURL + '" download="' + fileName + '">保存</a></td>';
+      filelist.appendChild(tr);
     }
     reader.readAsText(files[i], 'utf-8');
   }
